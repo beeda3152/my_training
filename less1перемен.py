ffa32868a5a6a89503d083063eca261f90e3fdff
beeda3152 <scherb.vladimir@yandex.ru>
2024-08-28 18:16:25 +0700

    Add files via upload

---
+++ less1перемен.py
@@ -0,0 +1,5 @@
+HW_count = 12
+hour_count = 1.5
+name = 'Python'
+class_time = hour_count / HW_count
+print("Курс:", name, ', всего задач:', HW_count, ', затрачено часов:', hour_count, ', среднее время выполнения ',class_time, " часа.")
