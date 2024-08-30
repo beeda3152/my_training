332ecaaff85ce03ad6928bdaac82404d28aaf527
beeda3152 <scherb.vladimir@yandex.ru>
2024-08-30 10:41:03 +0700

    Add files via upload

--- module_1_7.py
+++ module_1_7.py
@@ -1,7 +1,6 @@
 grades=[[5,3,3,5,4],[2,2,2,3],[4,5,5,2],[4,4,3],[5,5,5,4,5]]
 students={'Johnny','Bilbo', 'Steve', 'Khendrik', 'Aaron'}
 dict_stud={}
-sred_ball=[]
 list_stud = list(students)
 list_stud.sort()
 count_stud = len(list_stud)
