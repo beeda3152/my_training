422938d7203dfba40eb1f41265bd75323c0842a2
beeda3152 <scherb.vladimir@yandex.ru>
2024-09-14 20:50:43 +0700

    Add files via upload

--- module_3_hard.py
+++ module_3_hard.py
@@ -1,5 +1,4 @@
 def kl(ad):
-
     if ad == []:
         return ad
     if isinstance(ad[0], (tuple, set)):
@@ -9,6 +8,19 @@ def kl(ad):
     if isinstance(ad[0], list):
         return kl(ad[0]) + kl(ad[1:])
     return ad[:1] + kl(ad[1:])
+
+def kl1(lst):
+    if lst == []:
+        return 0
+    if isinstance(lst[0], (tuple, set)):
+        lst[0] = list(lst[0])
+    if isinstance(lst[0], dict):
+        lst[0] = [[key, value] for key, value in lst[0].items()]
+    if isinstance(lst[0], (list)):
+        return kl1(lst[0]) + kl1(lst[1:])
+    if isinstance(lst[0], str):
+        lst[0] = len(lst[0])
+    return int(*lst[:1]) + kl1(lst[1:])
 s = 0
 data_structure = [[1, 2, 3], {'a': 4, 'b': 5}, (6, {'cube': 7, 'drum': 8}),
                   "Hello", ((), [{(2, 'Urban', ('Urban2', 35))}])]
@@ -20,3 +32,4 @@ for i in a2:
         s += len(i)
 print(kl(a2))
 print(f'Сумма = {s}')
+print(kl1(data_structure))
