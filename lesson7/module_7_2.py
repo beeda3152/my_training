4bb2ddc3ae6674f3987d3c4bc6863c48fa580d7e
beeda3152 <scherb.vladimir@yandex.ru>
2024-10-14 13:33:54 +0700

    Add files via upload

--- lesson7/module_7_2.py
+++ lesson7/module_7_2.py
@@ -1,13 +1,3 @@
-import os
-def exists_file(fl):
-    if os.path.isfile(fl):
-        print(f"Файл {fl} существует и открыт для добавления")
-        file = open(fl, 'a', encoding='utf-8')
-    else:
-        print(f"Файл {fl} создан для записи.")
-        file = open(fl, 'w', encoding='utf-8')
-    return file
-
 def custom_write(file_name,strings):
     resul = {}
     n_str = 1
