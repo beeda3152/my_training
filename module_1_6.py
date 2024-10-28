ea40aa04ed44528d5fa66008b4aaca04dcaa9e3c
Владимир <scherb.vladimir@yandex.ru>
2024-10-28 12:52:25 +0700

    Add files via upload

--- module_1_6.py
+++ module_1_6.py
@@ -1,15 +1,15 @@
 my_dict = {'Max':1991, 'Nic':1992,'Nils':1993}
-print(my_dict)
-print(my_dict['Nic'])
-print(my_dict.get('Pit','Такого нет'))
+print('Dic: ', my_dict)
+print('Existing value:', my_dict['Nic'])
+print('Not existing value: ', my_dict.get('Pit'))
 my_dict.update({'Fil':1994, 'Mila':1995})
 print(my_dict)
 d_dict = my_dict.pop('Nic')
-print(d_dict)
-print(my_dict)
+print('Deleted value: ', d_dict)
+print('Modified dictionary: ', my_dict)
 #my_set = {True, 1, 2, 3, 's, ', 'sd',  2, 3, 's, ', 'sd', True}
-my_set = {1, 2, 'dfg', 1, 2,   2, 'dfg', 1}
+my_set = {1, 2, 'dfg', 1, 2, 2, (1, 2, 3), 'dfg', 1, False, (1, 2, 3)}
 print(my_set)
-my_set.update([5, 'sTr'])
+my_set.update([5, 'sTr', (1, 2, 3)])
 my_set.discard(2)
 print(my_set)
