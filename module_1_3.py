ac7f0e801125979c45e5e70b7d91e93321ab9607
Владимир <scherb.vladimir@yandex.ru>
2024-11-07 17:07:15 +0700

    Delete module_1_3.py
    g

--- module_1_3.py
+++
@@ -1,9 +0,0 @@
-name ='Владимир'
-print("Name:", name)
-age = 55
-print("Age:",  age)
-print('New age:', str(age) + " + 2")
-age = age + 2
-print("New age:", age)
-is_student = True
-print('Is Student', is_student)
