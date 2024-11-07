a55684c82f633a3b0262ddeb5c435249ac8be56e
Владимир <scherb.vladimir@yandex.ru>
2024-11-07 16:51:47 +0700

    Delete fake_math.pyg
    g

--- fake_math.py
+++
@@ -1,6 +0,0 @@
-def divide(first, second):
-    if second == 0:
-        return ('Ошибка. Деление на 0')
-    else:
-        c = first/second
-        return (f'{first} деленное на {second} раврно {c}')
