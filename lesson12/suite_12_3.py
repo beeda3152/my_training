ff46cc6cae1ef8fd32dc7df13e57f91ee5a305f8
Владимир <scherb.vladimir@yandex.ru>
2024-12-11 11:12:27 +0700

    Add files via upload

--- lesson12/suite_12_3.py
+++ lesson12/suite_12_3.py
@@ -1,10 +1,10 @@
 import unittest
-import module_12_1
-import module_12_2
+import tests_12_3
+
 
 uni_testST = unittest.TestSuite()
-uni_testST.addTest(unittest.TestLoader().loadTestsFromTestCase(module_12_1.RunnerTest))
-uni_testST.addTest(unittest.TestLoader().loadTestsFromTestCase(module_12_2.TournamentTest))
+uni_testST.addTest(unittest.TestLoader().loadTestsFromTestCase(tests_12_3.RunnerTest))
+uni_testST.addTest(unittest.TestLoader().loadTestsFromTestCase(tests_12_3.TournamentTest))
 
 runn = unittest.TextTestRunner(verbosity=2)
 runn.run(uni_testST)
