bed0d09a74f656c4943b3820549b1b767463e142
Владимир <scherb.vladimir@yandex.ru>
2025-01-17 08:23:26 +0700

    Add files via upload

--- lesson16/module_16_1.py
+++ lesson16/module_16_1.py
@@ -6,25 +6,25 @@ app = FastAPI()
 
 # маршрут к главной странице
 @app.get("/")
-async def main() -> dict:
-    return {"message": "Главная страница"}
+async def main() -> str:
+    return ("Главная страница")
 
 
 # маршрут к странице администратора
 @app.get("/user/admin")
-async def admin() -> dict:
-    return {"message": "Вы вошли как администратор"}
+async def admin() -> str:
+    return ("Вы вошли как администратор")
 
 
 # маршрут к страницам пользователей
 @app.get("/user/{user_id}")
-async def user(user_id: int) -> dict:
-    return {"message": f"Вы вошли как пользователь № {user_id}"}
+async def user(user_id: int) -> str:
+    return f"Вы вошли как пользователь № {user_id}"
 
 
 # маршрут к страницам пользователей
 @app.get("/user/")
-async def some_user(username: str, age: int) -> dict:
-    return {"message": f"Информация о пользователе. Имя: {username}, Возраст: {age}"}
+async def some_user(username: str, age: int) -> str:
+    return f"Информация о пользователе. Имя: {username}, Возраст: {age}"
 
 #user?username='Liliy'&age=123
