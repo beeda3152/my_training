bda64c9dce3320d48dad60cd39b2c846e21778b3
Владимир <scherb.vladimir@yandex.ru>
2025-02-01 11:38:03 +0700

    Add files via upload

---
+++ lesson17/module4/app/models/user.py
@@ -0,0 +1,22 @@
+from app.backend.db import Base
+from app.models import *
+from  sqlalchemy import Column, ForeignKey, Integer, String, Boolean,Float
+from  sqlalchemy.orm import relationship
+
+class User(Base):
+    __tablename__="users"
+    __table_args__ = {'extend_existing': True}
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String)
+    firstname = Column(String)
+    lastname = Column(String)
+    age = Column(Integer)
+    slug = Column(String, unique=True,index=True)
+    #is_active = Column(Boolean, default=True)
+    #parent_id = Column(Integer, ForeignKey('users.id'), nullable=True)
+
+    tasks = relationship("Task", back_populates='user')
+
+from sqlalchemy.schema import CreateTable
+print(CreateTable(User.__table__))
+
